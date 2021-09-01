226c0d375450fbb6edfc64cb3874fbac8430c3b8
Ben Sutherland <sutherland681@gmail.com>
2021-09-01 13:43:25 -0400

    Add inital functions for testing

--- video-editor-cli/src/main.rs
+++ video-editor-cli/src/main.rs
@@ -1,14 +1,16 @@
+use video_editor_lib::editor::Editor;
+
 extern crate video_editor_lib;
 fn main() {
-    let test = video_editor_lib::clip::Clip::new("bruh", 0, 0, 0, true, true);
-
-    let test2 = video_editor_lib::clip::Clip::new("bruh", 0, 0, 0, true, true);
-    
     let mut vector = Vec::new();
-    vector.push(test);
-    vector.push(test2);
 
     let timeline = video_editor_lib::timeline::Timeline {
         clips: vector
     };
+
+    let mut editor = Editor::new(timeline);
+
+    editor.init();
+
+    editor.compile("C:/Users/suthe/Desktop/Source/Test.mp4");
 }
