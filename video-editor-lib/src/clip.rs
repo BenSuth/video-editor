b945d860a4b834ae9ec35370e3eb849dffce3c50
Ben Sutherland <sutherland681@gmail.com>
2021-09-01 13:43:40 -0400

    modify clip structure

--- video-editor-lib/src/clip.rs
+++ video-editor-lib/src/clip.rs
@@ -1,23 +1,23 @@
-use ges::prelude::*;
-
 #[derive(Clone)]
 pub struct Clip {
-    pub name: String,
+    pub path: String,
     pub inpoint: u64,
     pub duration: u64,
     pub start: u64,
+    pub is_video: bool,
     pub is_audio: bool,
     pub is_render_target: bool,
-    clip: Option<ges::UriClip>
+    pub clip: Option<ges::UriClip>
 }
 
 impl Clip {
-    pub fn new(name: &str, inpoint: u64, duration: u64, start: u64, is_audio: bool, is_render_target: bool) -> Clip {
+    pub fn new(path: &str, inpoint: u64, duration: u64, start: u64, is_video: bool, is_audio: bool, is_render_target: bool) -> Clip {
         Clip {
-            name: name.to_string(),
+            path: path.to_string(),
             inpoint: inpoint,
             duration: duration,
             start: start,
+            is_video: is_video,
             is_audio: is_audio,
             is_render_target: is_render_target,
             clip: None
